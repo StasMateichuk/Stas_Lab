425fa50edc25223c68e31e08ee15e76cb21fb0fc
StasMateichuk <Stanislav.Mateichuk.TR.2022@lpnu.ua>
2023-06-07 15:47:57 +0300

    Lab_5

--- Lab_5.py
+++
@@ -1,7 +0,0 @@
-from num2word import word
-print("Hello")
-"""Asks for user integer number"""
-num = input("Enter number: ")
-
-"""Prints user number as text"""
-print("Your number is: "+ word(num))
